4995f40ef67001eaa12765597b8ec628c72e578f
Seunghoon Lee <lsh2002@hotmail.com>
2021-08-04 09:59:47 -0400

    Layout 1

--- UI/PartyGamesUI/src/app/app.module.ts
+++ UI/PartyGamesUI/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Layout1Component } from './layout1/layout1.component';
+import { Top1Component } from './top1/top1.component';
+import { Left1Component } from './left1/left1.component';
+import { Main1Component } from './main1/main1.component';
+import { Right1Component } from './right1/right1.component';
+import { Bottom1Component } from './bottom1/bottom1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Layout1Component,
+    Top1Component,
+    Left1Component,
+    Main1Component,
+    Right1Component,
+    Bottom1Component
   ],
   imports: [
     BrowserModule
---
+++ UI/PartyGamesUI/src/app/right1/right1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-right1',
+  templateUrl: './right1.component.html',
+  styleUrls: ['./right1.component.css']
+})
+export class Right1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
