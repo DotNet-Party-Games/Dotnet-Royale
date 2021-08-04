44920d4d6bcf14592f1fb32c94ec8bb69f3cfb50
Seunghoon Lee <lsh2002@hotmail.com>
2021-08-04 17:40:07 -0400

    Added service for user login

--- UI/PartyGamesUI/src/app/app.module.ts
+++ UI/PartyGamesUI/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { LoginComponent } from './login/login.component';
 import { LivechatComponent } from './livechat/livechat.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { LivechatComponent } from './livechat/livechat.component';
     LivechatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- UI/PartyGamesUI/src/app/login/login.component.ts
+++ UI/PartyGamesUI/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { PartygameService } from '../services/partygame.service';
+import { IUser } from '../services/user';
 
 @Component({
   selector: 'app-login',
@@ -6,15 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- modal:any = document.getElementById('id01');
-  constructor() { }
+
+  // modal:any = document.getElementById('id01');
+
+  currentUser: IUser;
+
+  userGroup = new FormGroup({
+    UserName: new FormControl(),
+    Password: new FormControl()
+  });
+
+  constructor(private partyGameApi: PartygameService) { }
 
   ngOnInit(): void {
+    let temp: IUser =
+    {
+      Id: 0,
+      UserName: "",
+      Password: "",
+      IsAdmin: false
+    }
+    this.currentUser = temp;
   }
-  // Get the modal
 
+  getUser(userGroup: FormGroup)
+  {
+    if(userGroup.get("username").value && userGroup.get("password").value)
+    {
+      this.partyGameApi.getUserByUserNameAndPassword(userGroup.get("username").value, userGroup.get("password").value).subscribe(
+        (response) => {
+          this.currentUser.Id = response.Id;
+          this.currentUser.UserName = response.UserName;
+          this.currentUser.Password = response.Password;
+          this.currentUser.IsAdmin = response.IsAdmin;
+        }
+      )
+    }
+  }
 
-// When the user clicks anywhere outside of the modal, close it
-
-
+  cancel()
+  {
+    document.getElementById('id01').style.display='none'
+  }
 }
