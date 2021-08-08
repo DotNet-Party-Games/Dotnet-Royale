3274b7d7319fdfe23eabd35b7342aff5639b2d71
klausLawson <lateklaus.lawsondjecky@revature.net>
2021-08-08 09:22:28 -0500

    remove the import of snake component which was creating errors in CICD

--- UI/PartyGamesUI/src/app/app.module.ts
+++ UI/PartyGamesUI/src/app/app.module.ts
@@ -18,7 +18,6 @@ import { LivechatComponent } from './livechat/livechat.component';
 import { GamelistComponent } from './gamelist/gamelist.component';
 import { RegisterComponent } from './register/register.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
-import { SnakeComponent } from './snake/snake.component';
 
 import { PartygameService } from './services/partygame.service';
 import { AuthGuard } from './services/auth.guard';
