4ee4f334b24ad6abaed9b78797ee6b1fc063ac4d
ThomasMoerschel <tj.moerschel@gmail.com>
2021-08-27 11:48:57 -0400

    Tron Bike/Snake Finished

--- UI/SocketServer/index.js
+++ UI/SocketServer/index.js
@@ -54,12 +54,12 @@ io.on('connection',(socket)=>{
 
     socket.on('gamestate', (data) =>{
         //console.log("gamestate data: " + JSON.stringify(data.GameState.snakePos));
-        socket.broadcast.to(data.room).emit('new gamestate',{b:data.GameState.snakePos, User: data.User, Score: data.Score, GameOver:data.GameState.lost});
+        socket.broadcast.to(data.room).emit('new gamestate',{b:data.SnakePos, User: data.User, Score: data.Score, GameOver:data.Lost});
         
     });
     socket.on('lightgamestate', (data) =>{
         //console.log("lightgamestate data: " + JSON.stringify(data.GameState.snakePos));
-        socket.broadcast.to(data.room).emit('new lightgamestate',{b:data.GameState.snakePos, User: data.User, Score: data.Score, GameOver:data.GameState.lost});
+        socket.broadcast.to(data.room).emit('new lightgamestate',{b:data.SnakePos, User: data.User, Score: data.Score, GameOver:data.Lost});
         
     });
 
